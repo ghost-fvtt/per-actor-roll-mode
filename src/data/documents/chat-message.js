0bbe136b8c82a1e4cd11a6feab2778df4492ba13
Johannes Loher <johannes.loher@fg4f.de>
2022-07-17 18:14:07 +0200

    feat: get rid of v10 deprecation warnings (#39)

--- src/data/documents/chat-message.js
+++ src/data/documents/chat-message.js
@@ -3,16 +3,21 @@
 // SPDX-License-Identifier: MIT
 
 import { packageId } from '../../constants.js';
+import { isGenerationOrLater } from '../../helpers.js';
 
 export const registerChatMessageFunctionality = () => {
   Hooks.on('preCreateChatMessage', preCreateChatMessage);
 };
 
 const preCreateChatMessage = (chatMessage, data, options) => {
-  const actor = ChatMessage.getSpeakerActor(chatMessage.data.speaker);
+  const actor = ChatMessage.getSpeakerActor(getSpeakerFromChatMessage(chatMessage));
   const rollMode = actor?.getFlag(packageId, 'roll-mode');
   if (rollMode !== undefined) {
     chatMessage.applyRollMode(rollMode);
     options.rollMode = rollMode;
   }
+};
+
+const getSpeakerFromChatMessage = (chatMessage) => {
+  return isGenerationOrLater(10) ? chatMessage.speaker : chatMessage.data.speaker;
 };
